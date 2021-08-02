ee70373e21afc68765624448706e64d78ce2c1fe
Seiichiro Ito <terryito0819@gmail.com>
2021-08-03 00:44:48 +0900

    update README and add github page

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,9 +1,16 @@
 import styled from "styled-components";
-
+import { AiOutlineGithub } from "react-icons/ai";
 const Header = () => {
   return (
     <HeaderStyled>
       <p>Pokedex</p>
+      <a
+        href="https://github.com/seiichiroito/Pokedex"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <AiOutlineGithub />
+      </a>
     </HeaderStyled>
   );
 };
@@ -11,10 +18,18 @@ const Header = () => {
 const HeaderStyled = styled.header`
   padding: 1rem 0;
   background: linear-gradient(to right, #0f2027, #203a43, #2c5364);
-
+  position: relative;
   p {
     font-size: 2em;
     text-align: center;
+    color: #f3f3f3;
+  }
+  svg {
+    position: absolute;
+    right: 2rem;
+    top: 50%;
+    transform: translateY(-50%);
+    font-size: 2rem;
     color: #f3f3f3;
   }
 `;
