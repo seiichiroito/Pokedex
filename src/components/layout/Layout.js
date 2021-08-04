ac23d50941f9260e6e709fe1ce676fc10f6b166a
Seiichiro Ito <terryito0819@gmail.com>
2021-08-05 01:49:09 +0900

    Add react-helmet and twitter meta

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,7 +1,7 @@
 import Header from "./Header";
 import Footer from "./Footer";
 import styled from "styled-components";
-
+import { Helmet } from "react-helmet";
 const LayoutStyled = styled.div`
   min-height: 100vh;
   display: flex;
@@ -13,6 +13,27 @@ const LayoutStyled = styled.div`
 const Layout = ({ children }) => {
   return (
     <LayoutStyled>
+      <Helmet>
+        <title>Pokedex</title>
+        <meta
+          name="description"
+          content="Pokedex - Search your favorite Pokemon"
+        />
+
+        {/* Twitter */}
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:creator" content="@seiichiro_ito" />
+        <meta name="twitter:url" content="https://sei-pokedex.vercel.app/" />
+        <meta name="twitter:title" content="Pokedex" />
+        <meta
+          name="twitter:description"
+          content="Pokedex - Search your favorite Pokemon"
+        />
+        <meta
+          name="twitter:image"
+          content="https://sei-pokedex.vercel.app/hero.png"
+        />
+      </Helmet>
       <Header />
       <main>{children}</main>
       <Footer />
